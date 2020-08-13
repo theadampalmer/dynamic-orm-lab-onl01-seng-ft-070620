82bfffb62d7398c06dfc25db67e2f863f5e00b56
Adam Palmer <theadampalmer@gmail.com>
2020-08-13 18:42:36 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,36 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+    def initialize(student)
+        @student = student
+    end
+    
+    def self.table_name
+        self.to_s.downcase.pluralize
+    end
+    
+    def self.column_name
+      
+    end
+    
+    def table_name_for_insert
+      
+    end
+    
+    def col_name_for_insert
+      
+    end
+    
+    def values_for_insert
+      
+    end
+    
+    def self.find_by_name
+        sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+        DB[:conn].execute(sql)
+    end
+    
+    def self.find_by
+      
+    end
 end
